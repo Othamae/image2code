3f0e96fb2e6fabab6e85560d4a1c0b2295e82682
Othamae <vero.gconesa@gmail.com>
2024-01-23 14:47:23 +0100

    refactor: :recycle: Created SideBar component

---
+++ components/SideBar.tsx
@@ -0,0 +1,16 @@
+
+const SideBar = () => {
+    return (
+        <aside className="flex flex-col justify-between min-h-screen p-4 bg-gray-900">
+            <header className="text-center mt-10">
+                <h1 className="text-3xl font-semibold">Image 2 Code</h1>
+                <h2 className="text-sm opacity-75">Generate code from an image</h2>
+            </header>
+
+            <section>{/*  Filtros */}</section>
+            <footer className="text-center ">Created by Othamae</footer>
+        </aside>
+    )
+}
+
+export default SideBar
